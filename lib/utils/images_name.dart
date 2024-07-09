215d70d7e9e00e7f797d4f89bbe505d548b1d433
Adham <adham30995@gmail.com>
2024-07-09 17:37:15 +0300

    first app

---
+++ lib/utils/images_name.dart
@@ -0,0 +1,4 @@
+const String appLogo = 'assets/images/home_page.png';
+const String background = 'assets/images/background.jpg';
+const String fingerprint = 'assets/images/fingerprint.png';
+const String alien = 'assets/images/alien.png';
